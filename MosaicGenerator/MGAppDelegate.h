79543544c51da5c351c0b91b772106e6395412ca
Nordling <emil.nordling@gmail.com>
2011-10-15 14:40:17 +0200

    First commit of MG

--- MosaicGenerator/MGAppDelegate.h
+++ MosaicGenerator/MGAppDelegate.h
@@ -1,15 +1,13 @@
-//
-//  MGAppDelegate.h
-//  MosaicGenerator
-//
-//  Created by Emil Nordling on 10/15/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
-@interface MGAppDelegate : NSObject <NSApplicationDelegate>
+@interface MGAppDelegate : NSObject <NSApplicationDelegate> {
+    NSImageView *imageView;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+
+@property (strong) IBOutlet NSImageView *imageView;
+
+-(IBAction) pickFile:(id)sender;
 
 @end
--- MosaicGenerator/MGAppDelegate.m
+++ MosaicGenerator/MGAppDelegate.m
@@ -1,20 +1,34 @@
-//
-//  MGAppDelegate.m
-//  MosaicGenerator
-//
-//  Created by Emil Nordling on 10/15/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import "MGAppDelegate.h"
 
 @implementation MGAppDelegate
 
 @synthesize window = _window;
+@synthesize imageView;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+}
+
+- (IBAction)pickFile:(id)sender {
+    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+    [openPanel setCanChooseFiles:YES];
+    [openPanel setCanChooseDirectories:NO];
+    [openPanel setAllowsMultipleSelection:NO];
+    [openPanel setCanCreateDirectories:NO];
+    
+    if ([openPanel runModal] ) {
+        NSArray *files = [openPanel URLs];
+        NSURL *selectedURL = [files lastObject];
+        if (selectedURL) {
+            NSImage *image = [[NSImage alloc] initWithContentsOfURL:selectedURL];
+            if (image) {
+                if ([image isValid]) {
+                    self.imageView.image = image;
+                }
+            }
+        }
+    }
 }
 
 @end
