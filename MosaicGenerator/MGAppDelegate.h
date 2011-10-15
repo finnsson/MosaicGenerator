4ad4691c9fce6804314450596b7438c568ca33e6
Nordling <emil.nordling@gmail.com>
2011-10-15 15:46:17 +0200

    Generator images read.

--- MosaicGenerator/MGAppDelegate.h
+++ MosaicGenerator/MGAppDelegate.h
@@ -2,12 +2,19 @@
 
 @interface MGAppDelegate : NSObject <NSApplicationDelegate> {
     NSImageView *imageView;
+    NSURL       *sourceImageURL;
+    NSMutableArray     *generatorImageURLs;    
 }
 
 @property (assign) IBOutlet NSWindow *window;
 
 @property (strong) IBOutlet NSImageView *imageView;
 
--(IBAction) pickFile:(id)sender;
+// Source Image: Input image
+// Album: Set of images to generate Target image from.
+// Target Image: 
+
+-(IBAction) pickSourceImage:(id)sender;
+-(IBAction) pickImages:(id)sender;
 
 @end
--- MosaicGenerator/MGAppDelegate.m
+++ MosaicGenerator/MGAppDelegate.m
@@ -1,5 +1,14 @@
 #import "MGAppDelegate.h"
 
+@interface MGAppDelegate(hidden) 
+
++ (BOOL)URLisFile:(NSURL*)url;
++ (BOOL)URLisDirectory:(NSURL*)url;
++ (BOOL)URLisValidImage:(NSURL*)url;
+
+@end
+
+
 @implementation MGAppDelegate
 
 @synthesize window = _window;
@@ -10,7 +19,7 @@
     // Insert code here to initialize your application
 }
 
-- (IBAction)pickFile:(id)sender {
+- (IBAction)pickSourceImage:(id)sender {
     NSOpenPanel *openPanel = [NSOpenPanel openPanel];
     [openPanel setCanChooseFiles:YES];
     [openPanel setCanChooseDirectories:NO];
@@ -29,6 +38,68 @@
             }
         }
     }
+}
+
+
++ (BOOL)URLisFile:(NSURL*)url
+{
+    NSNumber *isFile;
+    if([url getResourceValue:&isFile forKey:NSURLIsRegularFileKey error:NULL]) {
+        return [isFile intValue] == 1 ? YES : NO;
+    }
+    return NO;
+}
+
++ (BOOL)URLisDirectory:(NSURL*)url
+{
+    NSNumber *isDirectory;
+    if([url getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:NULL]) {
+        return [isDirectory intValue] == 1 ? YES : NO;
+    }
+    return NO;
+}
+
++ (BOOL)URLisValidImage:(NSURL*)url
+{
+    if ([MGAppDelegate URLisFile:url]) {
+        NSImage *image = [[NSImage alloc] initByReferencingURL:url];
+        if (image) {
+            if ([image isValid]) {
+                return YES;
+            }
+        }
+    }
+    return NO;
+}
+
+- (IBAction)pickImages:(id)sender {
+    generatorImageURLs = [[NSMutableArray alloc] initWithCapacity:100];
+    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+    [openPanel setCanChooseFiles:YES];
+    [openPanel setCanChooseDirectories:YES];
+    [openPanel setAllowsMultipleSelection:YES];
+    [openPanel setCanCreateDirectories:NO];
+    
+    if ([openPanel runModal] ) {
+        NSArray *selection = [openPanel URLs];
+        for (NSURL *url in selection) {
+            if ([MGAppDelegate URLisValidImage:url]) {
+                [generatorImageURLs addObject:url];
+            } else if([MGAppDelegate URLisDirectory:url]) {
+                NSDirectoryEnumerator *dirEnum = 
+                    [[NSFileManager defaultManager] enumeratorAtURL:url 
+                                         includingPropertiesForKeys: 
+                     [NSArray arrayWithObjects:NSURLNameKey, NSURLIsDirectoryKey, nil]
+                                                            options:NSDirectoryEnumerationSkipsHiddenFiles errorHandler:nil];
+                for (NSURL *subUrl in dirEnum) {
+                    if ([MGAppDelegate URLisValidImage:subUrl]) {
+                        [generatorImageURLs addObject:subUrl];
+                    }
+                }
+            }
+        }    
+    }
+    NSLog(@"Found %lu images", [generatorImageURLs count]);
 }
 
 @end
